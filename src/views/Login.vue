18165cb357a79a35ee8128f5bb729b0ab1fee63a
K.K <gokennsirou@yahoo.co.jp>
2020-06-21 07:35:16 +0900

    [Add] validation for login form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -10,68 +10,49 @@
           </article>
           <div class="column is-4 is-offset-4">
             <div class="box">
-              <div class="field">
-                <div class="control">
-                  <validation-provider rules="required" v-slot="{ errors }">
+              <validation-observer v-slot="{ invalid }" tag="form">
+                <div class="field">
+                  <validation-provider rules="required|email" v-slot="{ errors }" mode="lazy" name="メールアドレス">
                     <input class="input is-large" type="email" placeholder="Eメール" v-model="email" autofocus="" name="email">
                     <span>{{ errors[0] }}</span>
                   </validation-provider>
-                 </div>
-                 <!-- <div class="form-control-feedback" v-show="errors.has('email')">
-                  <p class="alert alert-danger">{{ errors.first('email') }}</p>
-                </div> -->
-              </div>
-              <div class="field">
-                <div class="control">
-                  <validation-provider rules="required" v-slot="{ errors }">
-                    <input class="input is-large" type="password" placeholder="パスワード" v-model="password" rule="'required|min:6|max:20'" maxlength="20" name="password">
+                </div>
+                <div class="field">
+                  <validation-provider rules="required|min:6|max:20" v-slot="{ errors }" mode="lazy" name="パスワード">
+                    <input class="input is-large" type="password" placeholder="パスワード" v-model="password" maxlength="20" name="password" autocomplete="on">
                     <span>{{ errors[0] }}</span>
                   </validation-provider>
                 </div>
-                <!-- <div class="form-control-feedback" v-show="errors.has('password')">
-                  <p class="alert alert-danger">{{ errors.first('password') }}</p>
-                </div> -->
-              </div>
-              <div class="field">
-                <label class="checkbox">
-                <input type="checkbox">
-                ログインしたままにする
-                </label>
-               </div>
-               <button class="button is-block is-info is-large is-fullwidth" @click="login()" >ログイン</button>
-               <!-- <button class="button is-block is-info is-large is-fullwidth" @click="login()" :disabled="!isValidated" >ログイン</button> -->
-              </div>
-              <p class="has-text-grey">
-                <a href="..">パスワードを忘れた方はこちら</a>
-              </p>
+                <div class="field">
+                  <label class="checkbox">
+                  <input type="checkbox">
+                  ログインしたままにする
+                  </label>
+                </div>
+                <button class="button is-block is-info is-large is-fullwidth" @click="login()" :disabled="invalid" >ログイン</button>
+              </validation-observer>
             </div>
+            <p class="has-text-grey">
+              <a href="..">パスワードを忘れた方はこちら</a>
+            </p>
           </div>
         </div>
+      </div>
     </section>
   </div>
 </template>
 
 <script>
-import {
-  // ValidationObserver,
-  ValidationProvider,
-  extend
-} from 'vee-validate'
-import { required } from "vee-validate/dist/rules"
+import { ValidationObserver, ValidationProvider } from 'vee-validate'
 import {
   CREATE,
   DESTROY
 } from '@/store/mutation-types'
 
-extend('required', {
-  ...required,
-  // message: '{_field_}は必須です'
-});
-
 export default {
   name: 'Login',
   components: {
-    // ValidationObserver,
+    ValidationObserver,
     ValidationProvider,
   },
   data() {
@@ -91,11 +72,7 @@ export default {
     }
   },
   computed: {
-    // isValidated () {
-    //   return Object.keys(this.fields).every(k => this.fields[k].validated) &&
-    //     Object.keys(this.fields).every(k => this.fields[k].valid)
-    // },
-    token () {
+    token() {
       return this.$store.state.auth.token
     },
     errorMessage () {
@@ -108,7 +85,6 @@ export default {
       // this.$router.push('/')
       console.log('ログイン済みです')
     }
-    console.log(this.fields)
   },
   // watch: {
   //   token (newToken) {
